066a497e450ca3fd7474e48c10c03a71868b1c51
Sergey Zhabinskii <sergey.zhabinskii@gmail.com>
2026-05-11 23:19:15 +0200

    Improve configuration

--- install.sh
+++ install.sh
@@ -28,11 +28,28 @@ $SUDO DEBIAN_FRONTEND=noninteractive apt-get install -y \
   zsh-autosuggestions \
   zsh-syntax-highlighting
 
-echo "==> Installing oh-my-zsh"
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+# echo "==> Installing oh-my-zsh"
+# if [ ! -d "$HOME/.oh-my-zsh" ]; then
+#   RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
+#     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# fi
+#
+#
+# echo "==> Installing .zshrc"
+# if [ -e "$HOME/.zshrc" ] && [ ! -L "$HOME/.zshrc" ]; then
+#   cp "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%s)"
+# fi
+# cp "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
+#
+# echo "==> Setting zsh as default shell"
+# ZSH_BIN="$(command -v zsh)"
+# if ! grep -qx "$ZSH_BIN" /etc/shells 2>/dev/null; then
+#   echo "$ZSH_BIN" | $SUDO tee -a /etc/shells >/dev/null
+# fi
+# current_shell="$(getent passwd "$USER" | cut -d: -f7)"
+# if [ "$(basename "$current_shell")" != "zsh" ]; then
+#   $SUDO chsh -s "$ZSH_BIN" "$USER"
+# fi
 
 echo "==> Installing neovim"
 ARCH="$(uname -m)"
@@ -50,12 +67,6 @@ $SUDO rm -rf "/opt/${NVIM_DIR}"
 $SUDO tar -C /opt -xzf "$TMP_TGZ"
 $SUDO ln -sf "/opt/${NVIM_DIR}/bin/nvim" /usr/local/bin/nvim
 
-# echo "==> Installing .zshrc"
-# if [ -e "$HOME/.zshrc" ] && [ ! -L "$HOME/.zshrc" ]; then
-#   cp "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%s)"
-# fi
-# cp "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
-
 echo "==> Cloning neovim configuration"
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
 mkdir -p "$HOME/.config"
@@ -63,15 +74,5 @@ if [ -d "$NVIM_CONFIG_DIR" ]; then
   mv "$NVIM_CONFIG_DIR" "${NVIM_CONFIG_DIR}.bak.$(date +%s)"
 fi
 git clone git@github.com:serzhby/nvim.git "$NVIM_CONFIG_DIR"
-
-# echo "==> Setting zsh as default shell"
-# ZSH_BIN="$(command -v zsh)"
-# if ! grep -qx "$ZSH_BIN" /etc/shells 2>/dev/null; then
-#   echo "$ZSH_BIN" | $SUDO tee -a /etc/shells >/dev/null
-# fi
-# current_shell="$(getent passwd "$USER" | cut -d: -f7)"
-# if [ "$(basename "$current_shell")" != "zsh" ]; then
-#   $SUDO chsh -s "$ZSH_BIN" "$USER"
-# fi
 
 echo "==> Done. Start a new shell or run: exec zsh"
